595259b6601a8eeba0b3e23f297019c24a684263
Yuta <yuta-10.11@outlook.jp>
2022-05-20 14:02:51 +0900

    'first'

---
+++ lib/main.dart
@@ -0,0 +1,285 @@
+// ignore_for_file: prefer_const_constructors, unused_element
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: const [
+          SideNavigation(),
+          VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: PostList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SideNavigation extends StatefulWidget {
+  const SideNavigation({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SideNavigationState createState() => _SideNavigationState();
+}
+
+class _SideNavigationState extends State<SideNavigation> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      // ignore: prefer_const_literals_to_create_immutables
+      destinations: [
+        const NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text('ThumbsUpDown'),
+        ),
+        NavigationRailDestination(
+          icon: const Icon(Icons.people),
+          label: const Text('People'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.face),
+          label: Text('Face'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.bookmark),
+          label: Text('Bookmark'),
+        ),
+      ],
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            leading: ClipOval(
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(
+                  Icons.storage,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            title: Text('Posts'),
+            subtitle: Text('20 Posts'),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: ListTile(
+            leading: ClipOval(
+              child: Container(
+                color: Colors.grey[300],
+                width: 48,
+                height: 48,
+                child: Icon(
+                  Icons.style,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            title: Text('All Types'),
+            subtitle: Text(''),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Post extends StatelessWidget {
+  final String name;
+  final String message;
+  final String textReason;
+  final Color colorPrimary;
+  final Color colorPositive;
+  final String textPositive;
+  final Color colorNegative;
+  final String textNegative;
+
+  const Post({
+    required Key key,
+    required this.name,
+    required this.message,
+    required this.textReason,
+    this.colorPrimary = Colors.blue,
+    this.colorPositive = Colors.blue,
+    required this.textPositive,
+    this.colorNegative = Colors.blue,
+    required this.textNegative,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      child: Card(
+        elevation: 8,
+        shadowColor: Colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              leading: ClipOval(
+                child: Container(
+                  color: colorPrimary,
+                  width: 48,
+                  height: 48,
+                  child: Center(
+                    child: Text(
+                      name.substring(0, 1),
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+              title: Text(name),
+              subtitle: Text('2 min ago'),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 72),
+                  Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: colorPrimary, width: 4),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Flexible(child: Text(message)),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: colorPrimary, width: 2),
+                      ),
+                    ),
+                    child: Text(
+                      textReason,
+                      style: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        primary: colorNegative,
+                      ),
+                      onPressed: () {},
+                      child: Text(textNegative),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        primary: colorPositive,
+                        backgroundColor: colorPositive.withOpacity(0.2),
+                      ),
+                      onPressed: () {},
+                      child: Text(textPositive),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PostList extends StatelessWidget {
+  const PostList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 48),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Header(),
+          Expanded(
+            child: ListView(
+              children: const [
+                Post(),
+                Post(),
+                Post(),
+                Post(),
+                Post(),
+                Post(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
